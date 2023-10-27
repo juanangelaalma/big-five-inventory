3decf980f5bcf086521cb90d0f2cd300c0731fa6
Juan Angela Alma <juanlah77@gmail.com>
2023-10-27 22:31:16 +0700

    Handle Title Guest Layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-guest-layout title="Login">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
