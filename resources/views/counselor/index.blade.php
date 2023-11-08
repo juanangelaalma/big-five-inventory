a8c211449cbcd9af6453d7d1f1b7df8dabed7133
Juan Angela Alma <juanlah77@gmail.com>
2023-11-08 15:12:18 +0700

    Add Counselor View

---
+++ resources/views/counselor/index.blade.php
@@ -0,0 +1,3 @@
+<x-counselor-layout pageTitle="Dashboard">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis voluptates aliquam, odit suscipit porro eum accusamus omnis illum expedita aliquid optio veniam ad autem ullam repudiandae dolorum cum laudantium hic.
+</x-counselor-layout>
---
+++ resources/views/layouts/counselor.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Windmill Dashboard</title> 
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+    <script src="{{ asset('js/init-alpine.js') }}"></script>
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
+    />
+    <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
+      defer
+    ></script>
+  </head>
+  <body>
+    <div
+      class="flex h-screen bg-gray-50 dark:bg-gray-900"
+      :class="{ 'overflow-hidden': isSideMenuOpen }"
+    >
+    <x-counselor.sidebar />
+      <div class="flex flex-col flex-1 w-full">
+        <x-counselor.header />
+        <main class="h-full overflow-y-auto">
+          <div class="container px-6 mx-auto grid">
+            @if ($pageTitle)
+              <x-counselor.page-title>{{ $pageTitle }}</x-counselor.page-title>
+            @endif
+
+            {{ $slot }}
+          </div>
+        </main>
+      </div>
+    </div>
+  </body>
+</html>
