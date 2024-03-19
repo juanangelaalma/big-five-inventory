f511e69cb31b9c77584a62ffbf836bd2754a2970
juanangelaalma <juanangelaalma@gmail.com>
2024-03-20 06:55:20 +0700

    Remove UNESA

--- resources/views/components/guest/footer.blade.php
+++ resources/views/components/guest/footer.blade.php
@@ -1,6 +1,5 @@
 <footer class="max-w-6xl mx-auto py-10">
     <div class="flex justify-center items-center space-x-2">
         <x-brand />
-        <p>Universitas Negeri Surabaya</p>
     </div>
 </footer>
